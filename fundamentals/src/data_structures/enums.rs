941ec28cf8c2ab10d6d738a93a1460bc781f56dc
shinobi5 <shinobi5@pm.me>
2020-09-06 21:32:15 +1200

    add enums and structs funcs

---
+++ fundamentals/src/data_structures/enums.rs
@@ -0,0 +1,38 @@
+enum Color {
+  Red, // unit-like struct
+  Green,
+  Blue,
+  RgbColor(u8, u8, u8), // tuple struct
+  CmykColor { // struct definition
+    cyan: u8,
+    magenta: u8,
+    yellow: u8,
+    black: u8,
+  },
+}
+
+pub fn enums() {
+  let c = Color::CmykColor {
+    cyan: 0,
+    magenta: 128,
+    yellow: 0,
+    black: 255,
+  };
+
+  match c {
+    Color::Red => println!("r"),
+    Color::Green => println!("g"),
+    Color::Blue => println!("b"),
+
+    Color::RgbColor(0, 0, 0)
+    | Color::CmykColor {
+        cyan: _,
+        magenta: _,
+        yellow: _,
+        black: 255,
+    } => println!("black"),
+
+    Color::RgbColor(r, g, b) => println!("rgb({},{},{}", r, g, b),
+    _ => (),
+  }
+}
